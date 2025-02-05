006b8e324ff6a79ad72aa9045a56e8d3a06df7fc
Tae5567 <eayodej1@umbc.edu>
2025-02-05 16:49:39 +0100

    Initial Commit

---
+++ backend/emergency-response/src/test/java/com/example/emergency_response/EmergencyResponseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emergency_response;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmergencyResponseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
